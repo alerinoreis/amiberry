d15a57ca439e9eabfadcb921f7ab324d30cb3441
Dimitris Panokostas <midwan@gmail.com>
2017-02-18 17:50:54 +0100

    Updated gencomp and generated files

--- VisualGDB/gencomp/Readme.txt
+++ VisualGDB/gencomp/Readme.txt
@@ -4,13 +4,9 @@ This tool generates 3 files:
 - comptbl.h
 
 To use it, compile it for the target system (e.g. Raspberry), then copy it there together
-with the "compiler" subdirectory and the "table68k" file.
+with the "jit/compiler" subdirectory and the "table68k" file.
 
-Execute the "gencomp" tool:
-
-gencomp > compemu.cpp
-
-, then copy back the generated files:
+Execute the "gencomp" tool, then copy back the generated files:
 - compemu.cpp
 - compstbl.cpp
 - comptbl.h
--- src/jit/compiler/gencomp_arm.cpp
+++ src/jit/compiler/gencomp_arm.cpp
@@ -3119,32 +3119,16 @@ gen_opcode(unsigned long int opcode)
 		genmov16(opcode, curi);
 		break;
 
-	case i_EMULOP_RETURN:
-		isjump;
-		failure;
-		break;
-
-	case i_EMULOP:
-		failure;
-		break;
-
-	case i_NATFEAT_ID:
-	case i_NATFEAT_CALL:
-		failure;
-		break;
-
-	case i_MMUOP:
-		//TODO
-//	case i_MMUOP030:
-//	case i_PFLUSHN:
-//	case i_PFLUSH:
-//	case i_PFLUSHAN:
-//	case i_PFLUSHA:
-//	case i_PLPAR:
-//	case i_PLPAW:
-//	case i_PTESTR:
-//	case i_PTESTW:
-//	case i_LPSTOP:
+	case i_MMUOP030:
+	case i_PFLUSHN:
+	case i_PFLUSH:
+	case i_PFLUSHAN:
+	case i_PFLUSHA:
+	case i_PLPAR:
+	case i_PLPAW:
+	case i_PTESTR:
+	case i_PTESTW:
+	case i_LPSTOP:
 		isjump;
 		failure;
 		break;
@@ -3279,11 +3263,11 @@ static char* outopcode(int opcode)
 		strcat(out, ".L");
 	strcat(out, " ");
 	if (ins->suse)
-		strcat(out, decodeEA(ins->smode, ins->size));
+		strcat(out, decodeEA(amodes(ins->smode), wordsizes(ins->size)));
 	if (ins->duse)
 	{
 		if (ins->suse) strcat(out, ",");
-		strcat(out, decodeEA(ins->dmode, ins->size));
+		strcat(out, decodeEA(amodes(ins->dmode), wordsizes(ins->size)));
 	}
 	return out;
 }
